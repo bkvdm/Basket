3931c576b4ca79972f60f0cc85ed4eb6fbcb64a3
bkvdm <vadim@991199.ru>
2023-11-18 10:45:33 +0300

    Домашняя работа №25 (задание по ссылке: https://my.sky.pro/student-cabinet/stream-lesson/75303/homework-requirements)

---
+++ src/main/java/tel/bvm/basket/HomeworkBasketApplication.java
@@ -0,0 +1,13 @@
+package tel.bvm.basket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkBasketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkBasketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tel/bvm/basket/HomeworkBasketApplicationTests.java
@@ -0,0 +1,13 @@
+package tel.bvm.basket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkBasketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
